2d770bd519f5f16c0d8f7276574b3793a4b30b26
Frederik Timme <frederik.t@gmx.de>
2021-07-23 16:55:18 +0200

    VotingWidget: an Datenstruktur angepasst; LefBarChart

---
+++ src/components/shared/charts/LefBarChart.js
@@ -0,0 +1,39 @@
+import { Bar, Line } from "react-chartjs-2";
+import React, { useContext } from "react";
+import { ThemeContext } from "../../theme/ThemeContext";
+
+const options = {
+  responsive: true,
+  scales: {
+    y: {
+      stacked: true,
+    },
+    x: {
+      stacked: true,
+    },
+  },
+};
+
+export function LefBarChart({ data }) {
+  const { theme } = useContext(ThemeContext);
+  const { DIAGRAM_COLORS = [] } = theme.colors;
+  return (
+    <Bar
+      data={{
+        labels: data.labels,
+        datasets: data.datasets.map((entry, i) => {
+          const color =
+            entry.color || DIAGRAM_COLORS[i % DIAGRAM_COLORS.length];
+          return {
+            backgroundColor: color,
+            borderColor: color,
+            label: entry.label,
+            data: entry.data,
+            yAxisID: entry.yAxisID,
+          };
+        }),
+      }}
+      options={options}
+    />
+  );
+}
--- src/components/widgets/VotingWidget.js
+++ src/components/widgets/VotingWidget.js
@@ -1,42 +1,141 @@
 import { Col, Row } from "react-bootstrap";
-import { LefLineChart } from "../shared/charts/LefLineChart";
 import React, { useEffect } from "react";
 import { requestGetVotingForDistrict } from "../../redux/votingSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { LefBarChart } from "../shared/charts/LefBarChart";
 
 const fakeElectionData = {
-  labels: [2005, 2010, 2015],
-  datasets: [
+  region: "Münster",
+  districtName: "Münster Innenstadt",
+  districtId: 123,
+  votingData: [
     {
-      label: "SPD",
-      data: [45.5, 50.5, 40.1],
+      year: 2018,
+      eligibleVoters: 1900,
+      voters: 2000,
+      validVotes: 1650,
+      partyResults: [
+        {
+          party: "SPD",
+          result: 90,
+        },
+        {
+          party: "CDU",
+          result: 10,
+        },
+      ],
     },
-    { label: "Grüne", data: [10.5, 21.5, 20.4] },
+    {
+      year: 2020,
+      eligibleVoters: 2900,
+      voters: 3000,
+      validVotes: 2650,
+      partyResults: [
+        {
+          party: "SPD",
+          result: 80,
+        },
+        {
+          party: "CDU",
+          result: 20,
+        },
+        {
+          party: "Grüne",
+          result: 50,
+        },
+      ],
+    },
+    {
+      year: 2022,
+      eligibleVoters: 2900,
+      voters: 3000,
+      validVotes: 2650,
+      partyResults: [
+        {
+          party: "SPD",
+          result: 70,
+        },
+        {
+          party: "Grüne",
+          result: 60,
+        },
+      ],
+    },
   ],
 };
 
-export const VotingWidget = ({ votingId, districtId, districtName }) => {
+const partyColors = {
+  SPD: `rgb(220,50,50)`,
+  Grüne: `rgb(50,160,50)`,
+  CDU: `rgb(50,50,50)`,
+};
+
+const getPartyColor = (party) => partyColors[party];
+
+export const VotingWidget = ({ regionData = {} }) => {
   const dispatch = useDispatch();
+  const { votingId, districtId, districtName } = regionData;
   useEffect(() => {
-    dispatch(requestGetVotingForDistrict(votingId, districtId, districtName));
+    if (votingId || districtId || districtName)
+      dispatch(requestGetVotingForDistrict(votingId, districtId, districtName));
   });
 
-  const votingData = useSelector((state) => state.voting[votingId]);
+  const electionData = useSelector((state) => state.voting[votingId]) || {};
+  const {
+    // region,
+    votingData = [],
+    districtName: electionDistrictName,
+  } = electionData;
+  let partyData = {};
+  let labels = [];
+  votingData.forEach((voting) => {
+    const {
+      // eligibleVoters,
+      // voters,
+      // validVotes,
+      year,
+      partyResults = [],
+    } = voting;
+    labels.push(year);
+    partyResults.forEach((partyResult) => {
+      const { party, result } = partyResult;
+      partyData[party] = [...(partyData[party] || []), { year, result }];
+    });
+  });
+
+  const data = {
+    labels: labels,
+    datasets: Object.keys(partyData).map((key) => ({
+      color: getPartyColor(key),
+      label: key,
+      data: labels.map((year) => {
+        const partyEntry = partyData[key] || {};
+        const partyEntryForYear = partyEntry.find((d) => d.year === year);
+        return partyEntryForYear ? partyEntryForYear.result : 0;
+      }),
+    })),
+  };
 
   return (
     <Col>
-      <Row>
-        <Col sm={12} lg={8}>
-          <div style={{ width: "100%" }}>
-            <LefLineChart data={votingData || fakeElectionData} />
-          </div>
-        </Col>
-        <Col sm={12} lg={4} className={"mt-sm-2"}>
+      {votingData.length > 0 ? (
+        <Row>
+          <Col sm={12} lg={12}>
+            <div style={{ width: "100%" }}>
+              <LefBarChart data={data} />
+            </div>
+          </Col>
+          {/*<Col sm={12} lg={4} className={"mt-sm-2"}>
           <p
             style={{ whiteSpace: "pre-wrap" }}
-          >{`Die stärkste Partei ist derzeit die SPD mit 40 Prozent.\n\nParteien mit einem inhaltlichen Schwerpunkt auf das Thema Klimawandel haben in den letzten Jahrzehnten an Bedeutung erlangt.`}</p>
-        </Col>
-      </Row>
+          >{`Die stärkste Partei bei der letzten Wahl in ${electionDistrictName} war die Partei "${mostRecentWinner}" mit ${mostRecentWinnerResult} Prozent.`}</p>
+        </Col>*/}
+        </Row>
+      ) : (
+        <p className={"text-center mt-2 alert alert-secondary"}>
+          Keine Daten vorhanden.
+        </p>
+      )}
     </Col>
   );
 };
